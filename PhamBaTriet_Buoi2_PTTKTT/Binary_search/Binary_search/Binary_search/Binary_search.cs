9a954ff1ab806b578ca7b0a12e54a5ec70032acd
Pham Ba Triet <trietqnsh@gmail.com>
2024-07-17 10:47:42 +0700

    first commit

---
+++ PhamBaTriet_Buoi2_PTTKTT/Binary_search/Binary_search/Binary_search/Binary_search.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    class Binary_search
+    {
+       
+        public static int BinarySearchIterative(int[] array, int key)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+               
+                if (array[mid] == key)
+                    return mid;
+
+              
+                if (array[mid] > key)
+                    right = mid - 1;
+                else 
+                    left = mid + 1;
+            }
+
+           
+            return -1;
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 10, 40 };
+            int key = 10;
+
+            int result = BinarySearchIterative(array, key);
+
+            if (result != -1)
+                Console.WriteLine("Phan tu duoc tim thay tai vi tri: " + result);
+            else
+                Console.WriteLine("Phan tu khong co trong mang");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PhamBaTriet_Buoi2_PTTKTT/Merge_sort/Merge_sort/Merge_sort/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_sort
+{
+    class Program
+    {
+        
+        static void Merge(int[] arr, int left, int middle, int right)
+        {
+            int n1 = middle - left + 1;
+            int n2 = right - middle;
+
+            
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+      
+            for (int i = 0; i < n1; ++i)
+                L[i] = arr[left + i];
+            for (int j = 0; j < n2; ++j)
+                R[j] = arr[middle + 1 + j];
+
+          
+            int k = left;
+            int p = 0, q = 0;
+            while (p < n1 && q < n2)
+            {
+                if (L[p] <= R[q])
+                {
+                    arr[k] = L[p];
+                    p++;
+                }
+                else
+                {
+                    arr[k] = R[q];
+                    q++;
+                }
+                k++;
+            }
+
+          
+            while (p < n1)
+            {
+                arr[k] = L[p];
+                p++;
+                k++;
+            }
+
+            
+            while (q < n2)
+            {
+                arr[k] = R[q];
+                q++;
+                k++;
+            }
+        }
+
+        
+        static void Sort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                
+                Sort(arr, left, middle);
+                Sort(arr, middle + 1, right);
+
+                
+                Merge(arr, left, middle, right);
+            }
+        }
+
+        
+        static void InMang(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Mang ban dau:");
+            InMang(arr);
+
+            Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nMang da sap xep:");
+            InMang(arr);
+        }
+    }
+}
